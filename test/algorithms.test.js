77a6110ab07c8b864230a25774226d9c5f29f807
kneuroth <kneuroth@uwo.ca>
2022-02-10 14:18:37 -0500

    Added more test cases for haveSameData algorithm

--- test/algorithms.test.js
+++ test/algorithms.test.js
@@ -28,13 +28,28 @@ describe('Algorithms', () => {
             assert.equal(Algorithms.getLowestAvailableInt([-1, 2, 0, 1, 3, -3]), 4); 
         })
         it('should return 0 when given an empty array', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([]), 0)
+            assert.equal(Algorithms.getLowestAvailableInt([]), 0);
         })
 
     })
     describe('#haveSameData', () => {
         it('should return true for { A: 1 } and { A: 1}', () => {
-            assert.equal(Algorithms.haveSameData({A: 1}, {A: 1}), true)
+            assert.equal(Algorithms.haveSameData({A: 1}, {A: 1}), true);
+        })
+        it('should return false for { A: 1 } and { A: 2 }', () => {
+            assert.equal(Algorithms.haveSameData({A: 1}, {A: 2}), false);
+        })
+        it('should return false for { A: 1 } and { A: 1, B: 1 }', () => {
+            assert.equal(Algorithms.haveSameData({A: 1}, {A: 1, B: 1}), false);
+        })
+        it('should return false for { A: 1, B: 1 } and { A: 1 }', () => {
+            assert.equal(Algorithms.haveSameData({A: 1, B: 1}, {A: 1}), false);
+        })
+        it('should return false for { A: 1, B: 1 } and { A: 1, B: 2 }', () => {
+            assert.equal(Algorithms.haveSameData({A: 1, B: 1}, {A: 1, B: 2}), false);
+        })
+        it('should return true for {} and {}', () => {
+            assert.equal(Algorithms.haveSameData({}, {}), true);
         })
     })
 })
