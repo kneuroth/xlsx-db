70eb58318c8a8d6124f3b40d5920427877d4bd82
Kelly Neuroth <kelly.neuroth@outlook.com>
2022-02-10 16:54:31 -0500

    Changed algorithm name
    Adds more descriptive tests

--- test/algorithms.test.js
+++ test/algorithms.test.js
@@ -5,51 +5,57 @@ import Algorithms from "../lib/js/algorithms.js";
 
 
 describe('Algorithms', () => {
-    describe('#getLowestAvailableInt', () => {
-        it('should return 1 when given [0, 2, 3]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([0, 2, 3]), 1);    
+    describe('#getLowestIntNotIn', () => {
+        it('Returns 1 when given [0, 2, 3]. Handles case when the lowest int is between 2 numbers in the array..', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([0, 2, 3]), 1);    
         })
-        it('should return 0 when given [1, 2, 3]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([1, 2, 3]), 0); 
+        it('Returns 0 when given [1, 2, 3]. Handles case when lowest int is below the lowest in the array.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([1, 2, 3]), 0); 
         })
-        it('should return 0 when given [3, 2, 1]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([3, 2, 1]), 0); 
+        it('Returns 0 when given [3, 2, 1]. Handles case when lowest int not in array is below lowest in the array and array is not sorted.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([3, 2, 1]), 0); 
         })
-        it('should return 1 when given [3, 0, 2]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([3, 0, 2]), 1); 
+        it('Returns 1 when given [3, 0, 2]. Handles case when lowest int not in array is between 2 numbers and the array is not sorted.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([3, 0, 2]), 1); 
         })
-        it('should return 4 when given [0, 1, 2, 3]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([0, 1, 2, 3]), 4); 
+        it('Returns 4 when given [0, 1, 2, 3]. Handles case when lowest int not in array is greater than highest int in array.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([0, 1, 2, 3]), 4); 
         })
-        it('should return 0 when given [-1, -2, -3]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([-1, -2, -3]), 0); 
+        it('Returns 0 when given [-1, -2, -3]. Hanldes case when array is all negative.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([-1, -2, -3]), 0); 
         })
-        it('should return 4 when given [-1, 2, 0, 1, 3, -3]', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([-1, 2, 0, 1, 3, -3]), 4); 
+        it('Returns 4 when given [-1, 2, 0, 1, 3, -3]. Handles case when array is mixed negative and positive integers.', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([-1, 2, 0, 1, 3, -3]), 4); 
         })
-        it('should return 0 when given an empty array', () => {
-            assert.equal(Algorithms.getLowestAvailableInt([]), 0);
+        it('Returns 0 when given an empty array. Handles case when array is empty (lowest int not in an empty array is 0).', () => {
+            assert.equal(Algorithms.getLowestIntNotIn([]), 0);
         })
 
     })
     describe('#haveSameData', () => {
-        it('should return true for { A: 1 } and { A: 1}', () => {
+        it('Returns true for { A: 1 } and { A: 1}. Handles case for 2 identical objects.', () => {
             assert.equal(Algorithms.haveSameData({A: 1}, {A: 1}), true);
         })
-        it('should return false for { A: 1 } and { A: 2 }', () => {
+        it('Returns false for { A: 1 } and { A: 2 }. Handles case for 2 objects with different data but same key.', () => {
             assert.equal(Algorithms.haveSameData({A: 1}, {A: 2}), false);
         })
-        it('should return false for { A: 1 } and { A: 1, B: 1 }', () => {
+        it('Returns false for { A: 1 } and { A: 1, B: 1 }. Handles case where B is the same as A but with an extra property.', () => {
             assert.equal(Algorithms.haveSameData({A: 1}, {A: 1, B: 1}), false);
         })
-        it('should return false for { A: 1, B: 1 } and { A: 1 }', () => {
+        it('Returns false for { A: 1, B: 1 } and { A: 1 }. Handles case where A is the same as B but with an extra property.', () => {
             assert.equal(Algorithms.haveSameData({A: 1, B: 1}, {A: 1}), false);
         })
-        it('should return false for { A: 1, B: 1 } and { A: 1, B: 2 }', () => {
+        it('Returns false for { A: 1, B: 1 } and { A: 1, B: 2 }. Handles case where 1 set of values is different.', () => {
             assert.equal(Algorithms.haveSameData({A: 1, B: 1}, {A: 1, B: 2}), false);
         })
-        it('should return true for {} and {}', () => {
+        it('Returns true for {} and {}. Handles case where neither A or B have any properties.', () => {
             assert.equal(Algorithms.haveSameData({}, {}), true);
+        })
+        it('Returns false for { A: 1 } and {}. Handles case where B has no properties.', () => {
+            assert.equal(Algorithms.haveSameData({ A: 1 }, {}), false);
+        })
+        it('Returns false for {} and { B : 1 }. Handles case where A has no properties.', () => {
+            assert.equal(Algorithms.haveSameData({}, { B: 1 }), false);
         })
     })
 })
--- test/methods.test.js
+++ test/methods.test.js
@@ -11,19 +11,19 @@ describe('Methods', () => {
     
     describe('#post', () => {
         
-        it('should add a single record to empty.xlsx', () => {
+        it('Adds a single record to empty.xlsx', () => {
             let result = methods.post({ test: "A" }, 'testFiles/empty.xlsx');
             assert.equal(result.id, 0);
             assert.equal(result.test, "A");
         })
 
-        it('should add a single record to hasOne.xlsx with id 1', () => {
+        it('Adds a single record to hasOne.xlsx with id 1', () => {
             let result = methods.post({ test: "A" }, 'testFiles/hasOne.xlsx');
             assert.equal(result.id, 1);
             assert.equal(result.test, "A");
         })
 
-        it('should throw an error when passing a non-existant filepath', () => {
+        it('Throws an error when passing a non-existant filepath', () => {
             assert.throws(() => { methods.post({ test: "A" }, 'testFiles/nonexistant.xlsx') });
         })
 
@@ -31,7 +31,7 @@ describe('Methods', () => {
     })
 
     describe('#find', () => {
-        it('should return all records in a sheet', () => {
+        it('Returns all records in a sheet', () => {
             let result = methods.find('testfiles/findAll.xlsx');
             let expected = [
                 {
